c064c16cc68fd2a6fa01524762588f8effe08c60
Megan Coyle <meg@mcoyle.com>
2016-11-18 17:16:07 -0500

    Add ability to toggle details

--- app/Card/index.js
+++ app/Card/index.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import CheckList from '../CheckList';
 
 class Card extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showDetails: false
+    };
+  }
+
   render() {
+    let cardDetails;
+      if (this.state.showDetails) {
+        cardDetails = (
+          <div className="card_details">
+            {this.props.description}
+            <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+          </div>
+        );
+      };
     return (
       <div className="card">
-        <div className="card_title">{this.props.title}</div>
-        <div className="card_details">
-          {this.props.description}
-          <CheckList cardId={this.props.id} tasks={this.props.tasks} />
-        </div>
+        <div className="card_title" onClick={
+          ()=>this.setState({showDetails: !this.state.showDetails})
+        }>{this.props.title}</div>
+        {cardDetails}
        </div>
     );
   }
