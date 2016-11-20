37b3cdd2b86fa93e523278fc1474dc60c6610297
Megan Coyle <meg@mcoyle.com>
2016-11-20 17:55:14 -0500

    Add custom proptype validator

--- app/Card/index.js
+++ app/Card/index.js
@@ -2,6 +2,17 @@ import React, { Component, PropTypes } from 'react';
 import CheckList from '../CheckList';
 import marked from 'marked';
 
+let titlePropType = (props, propName, componentName) => {
+  if (props[propName]) {
+    let value = props[propName];
+    if (typeof value !== 'string' || value.length > 80) {
+      return new Error(
+        `${propName} in ${componentName} is longer than 80 characters`
+      );
+    }
+  }
+}
+
 class Card extends Component {
   constructor() {
     super(...arguments);
@@ -51,6 +62,9 @@ class Card extends Component {
 
 Card.propTypes = {
   cardId: PropTypes.number,
+  title: titlePropType,
+  description: PropTypes.string,
+  color: PropTypes.string,
   tasks: PropTypes.arrayOf(PropTypes.object)
 };
 
