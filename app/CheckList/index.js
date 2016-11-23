9ee062b3002c6d384d2dfd5da43e0ba5fb65a839
Megan Coyle <meg@mcoyle.com>
2016-11-22 19:25:50 -0500

    Attempt to incorporate taskcallbacks to checklist

--- app/CheckList/index.js
+++ app/CheckList/index.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class CheckList extends Component {
+  checkInputKeyPress(evt){
+    if(evt.key === 'Enter'){
+      this.props.taskCallbacks.add(this.props.cardId, evt.target.value);
+      evt.target.value = '';
+    }
+  }
+
   render() {
-    let tasks = this.props.tasks.map((task) => (
+    let tasks = this.props.tasks.map((task, taskIndex) => (
       <li key={task.id} className="checklist_task">
-        <input type="checkbox" defaultChecked={task.done} />
-        {task.name}
-        <a href="#" className="checklist_task--remove" />
+        <input type="checkbox" checked={task.done} onChange={
+          this.props.taskCallbacks.toggle.bind(null, this.props.cardId, task.id, taskIndex)
+        } />
+        {task.name}{ ' '}
+        <a href="#" className="checklist_task--remove" onClick={
+          this.props.taskCallbacks.delete.bind(null, this.props.cardId, task.id, taskIndex)
+        } />
       </li>
     ));
 
@@ -15,10 +26,17 @@ class CheckList extends Component {
         <ul>{tasks}</ul>
         <input type="text"
                className="checklist--add-task"
-               placeholder="Type then hit Enter to add a task" />
+               placeholder="Type then hit Enter to add a task"
+               onKeyPress={this.checkInputKeyPress.bind(this)} />
       </div>
     );
   }
 }
+
+CheckList.propTypes = {
+  cardId: PropTypes.number,
+  taskCallbacks: PropTypes.object,
+  tasks: PropTypes.array
+};
 
 export default CheckList;
--- app/List/index.js
+++ app/List/index.js
@@ -3,13 +3,14 @@ import Card from '../Card';
 
 class List extends Component {
   render() {
-    var cards = this.props.cards.map((card) => {
+    let cards = this.props.cards.map((card) => {
       return <Card key={card.id}
-                  id={card.id}
+                  taskCallbacks={this.props.taskCallbacks}
+                  {id={card.id}
                   title={card.title}
                   description={card.description}
                   color={card.color}
-                  tasks={card.tasks} />
+                  tasks={card.tasks}} />
     });
 
     return (
@@ -23,7 +24,8 @@ class List extends Component {
 
 List.propTypes = {
   title: PropTypes.string.isRequired,
-  cards: PropTypes.arrayOf(PropTypes.object)
+  cards: PropTypes.arrayOf(PropTypes.object),
+  taskCallbacks: PropTypes.object,
 };
 
 export default List;
